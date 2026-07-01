f5872d75115d4f6d1d0b5ce5db18c4eb589b1d54
torak323 <154097385+torak323@users.noreply.github.com>
2026-07-01 09:13:14 +0200

    Add files via upload

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,33 +1,14 @@
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// ---- Basis ----
-#ifndef USE_I2C
+// Enable I2C support
 #define USE_I2C
-#endif
 
-// ---- PCF8574 / PCF8574A GPIO Expander ----
-#ifndef USE_PCF8574
+// Enable PCF8574 / PCF8574A I/O expander support
 #define USE_PCF8574
-#endif
-
-// Optional: PCF8574 Eingänge als Sensor/MQTT nutzbar machen
-#ifndef USE_PCF8574_SENSOR
 #define USE_PCF8574_SENSOR
-#endif
-
-#ifndef USE_PCF8574_MQTTINPUT
-#define USE_PCF8574_MQTTINPUT
-#endif
-
-// Optional: PCF8574 als Display/Input-Variante nutzbar
-#ifndef USE_PCF8574_DISPLAYINPUT
 #define USE_PCF8574_DISPLAYINPUT
-#endif
-
-// Optionaler PCF8574 Mode 2 Support
-#ifndef USE_PCF8574_MODE2
+#define USE_PCF8574_MQTTINPUT
 #define USE_PCF8574_MODE2
-#endif
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
